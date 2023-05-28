68d89ac95e020faa94082a27e57629cfd87025fb
fikireab-m <fikireab.meku@gmail.com>
2023-05-28 15:53:14 +0300

    added logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,8 +12,8 @@ export default function Header() {
             <span className="sr-only">Your Company</span>
             <img
               className="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt=""
+              src="logo.png"
+              alt="company logo"
             />
           </a>
         </div>
--- src/pages/JoinOrCreateRoom.jsx
+++ src/pages/JoinOrCreateRoom.jsx
@@ -4,7 +4,7 @@ export default function JoinOrCreateRoom() {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img className="mx-auto h-10 w-auto" src="vite.svg" alt="DevChat" />
+        <img className="mx-auto h-10 w-auto" src="logo.png" alt="DevChat" />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Join Or Create a room
         </h2>
