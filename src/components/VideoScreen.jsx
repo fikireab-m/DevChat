f2823dc7b777d154bf1b09e6dafe54e3542cd506
fikireab-m <fikireab.meku@gmail.com>
2023-05-28 15:07:44 +0300

    added agora video

--- src/components/VideoScreen.jsx
+++ src/components/VideoScreen.jsx
@@ -1,6 +1,7 @@
-// import AgoraUIKit from "agora-react-uikit";
 // import { useState } from "react";
-// export default function VideoScreen() {
+// import AgoraUIKit from "agora-react-uikit";
+
+// const VideoScreen = () => {
 //   const [videoCall, setVideoCall] = useState(true);
 //   const rtcProps = {
 //     appId: "4f79278a28834b9982933591d900ff81",
@@ -12,92 +13,99 @@
 //   };
 //   return (
 //     <div className="m-auto">
-//     {videoCall ? (
-//       <div style={{ display: "flex", width: "calc(100vw/2.1)", height: "360px" }}>
-//         <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
-//       </div>
-//     ) : (
-//       <button onClick={() => setVideoCall(true)}>Join</button>
-//     )}
-//   </div>
-//     // <main>
-//     //   <div className="h-[calc(100vh-80px)] bg-gray-300  flex flex-col mx-auto p-4">
-//     //     <div className="flex items-center justify-center self-center">
-//     //       <button>Button 1</button>
-//     //       <button>Button 1</button>
-//     //       <button>Button 1</button>
-//     //       <button>Button 1</button>
-//     //     </div> 
-//     //   </div>
-//     // </main>
+//       {videoCall ? (
+//         <div className="flex w-[calc(100vw/2.1)] h-[360px]">
+//           <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+//         </div>
+//       ) : (
+//         <button onClick={() => setVideoCall(true)}>Join</button>
+//       )}
+//     </div>
 //   );
-// }
+// };
 
+// export default VideoScreen;
 
-import { useEffect, useRef } from 'react';
-import AgoraRTC from 'agora-rtc-sdk';
+import { useState } from "react";
+import AgoraUIKit, { layout } from "agora-react-uikit";
+import "agora-react-uikit/dist/index.css";
 
-const VideoChat = () => {
-  const agoraVideoPlayerRef = useRef(null);
-
-  useEffect(() => {
-    // Join the video chat channel
-    // Replace <CHANNEL_NAME> with your desired channel name
-    const joinChannel = async () => {
-      // Create an AgoraRTC client
-      const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-
-      // Initialize the AgoraRTC client
-      await client.init('4f79278a28834b9982933591d900ff81');
-
-      // Join the AgoraRTC channel
-      await client.join(null, 'Fikir', null, '0001');
-
-      // Set up the video player to display remote video streams
-      const videoPlayer = AgoraRTC.createPlayer();
-      videoPlayer.init();
-
-      // Subscribe to remote streams
-      client.on('stream-added', ({ stream }) => {
-        client.subscribe(stream);
-      });
-
-      client.on('stream-subscribed', ({ stream }) => {
-        videoPlayer.play(stream.getId());
-      });
-
-      // Publish the local stream
-      const localStream = AgoraRTC.createStream({
-        audio: false,
-        video: true,
-      });
-
-      await localStream.init();
-      await client.publish(localStream);
-
-      // Set up the video player to display the local stream
-      localStream.play(agoraVideoPlayerRef.current);
-    };
-
-    joinChannel();
-
-    return () => {
-      // Leave the video chat channel
-      const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
-      client.leave();
-
-      // Stop playing the video stream
-      const videoPlayer = AgoraRTC.createPlayer();
-      videoPlayer.stop();
-    };
-  }, []);
+const VideoScreen = () => {
+  const [videocall, setVideocall] = useState(true);
+  const [isHost, setHost] = useState(true);
+  const [isPinned, setPinned] = useState(false);
+  const [username, setUsername] = useState("");
 
   return (
-    <div className="flex flex-wrap justify-center">
-      <div className="w-1/2 h-screen" ref={agoraVideoPlayerRef}></div>
-      {/* Additional UI elements can be added here */}
+    <div className="flex flex-1 w-[calc(75vw)-100px] h-[calc(90vh)]">
+      <div style={styles.videoContainer}>
+        {videocall ? (
+          <>
+            <div style={styles.nav}>
+              <p style={{ fontSize: 20, width: 200 }}>
+                You are {isHost ? "a host" : "an audience"}
+              </p>
+              <p style={styles.btn} onClick={() => setHost(!isHost)}>
+                Change Role
+              </p>
+              <p style={styles.btn} onClick={() => setPinned(!isPinned)}>
+                Change Layout
+              </p>
+            </div>
+            <AgoraUIKit
+              rtcProps={{
+                appId: "4f79278a28834b9982933591d900ff81",
+                channel: "test",
+                token: null, // add your token if using app in secured mode
+                role: isHost ? "host" : "audience",
+                layout: isPinned ? layout.pin : layout.grid,
+                enableScreensharing: true,
+                grid:2
+              }}
+              rtmProps={{ username: username || "user", displayUsername: true }}
+              callbacks={{
+                EndCall: () => setVideocall(false),
+                ChageView:()=>setPinned(!isPinned),
+              }}
+            />
+          </>
+        ) : (
+          <div style={styles.nav}>
+            <input
+              style={styles.input}
+              placeholder="nickname"
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+            <h3 style={styles.btn} onClick={() => setVideocall(true)}>
+              Start Call
+            </h3>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
 
-export default VideoChat;
+const styles = {
+  videoContainer: {
+    display: "flex",
+    flexDirection: "column",
+    flex: 1,
+  },
+  nav: { display: "flex", justifyContent: "space-around" },
+  btn: {
+    backgroundColor: "#007bff",
+    cursor: "pointer",
+    borderRadius: 5,
+    padding: "4px 8px",
+    color: "#ffffff",
+    fontSize: 20,
+  },
+  input: { display: "flex", height: 24, alignSelf: "center" },
+};
+
+export default VideoScreen;
--- src/pages/ChatRoom.jsx
+++ src/pages/ChatRoom.jsx
@@ -1,60 +1,7 @@
 import ChatsScreen from "../components/ChatScreen";
-import ListOfUsers from "../components/ListOfUsers";
+import Header from "../components/Header";
 import VideoScreen from "../components/VideoScreen";
-const people = [
-  {
-    name: "Leslie Alexander",
-    email: "leslie.alexander@example.com",
-    role: "Co-Founder / CEO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    lastSeen: "3h ago",
-    lastSeenDateTime: "2023-01-23T13:23Z",
-  },
-  {
-    name: "Michael Foster",
-    email: "michael.foster@example.com",
-    role: "Co-Founder / CTO",
-    imageUrl:
-      "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    lastSeen: "3h ago",
-    lastSeenDateTime: "2023-01-23T13:23Z",
-  },
-  {
-    name: "Dries Vincent",
-    email: "dries.vincent@example.com",
-    role: "Business Relations",
-    imageUrl:
-      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    lastSeen: null,
-  },
-  {
-    name: "Lindsay Walton",
-    email: "lindsay.walton@example.com",
-    role: "Front-end Developer",
-    imageUrl:
-      "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    lastSeen: "3h ago",
-    lastSeenDateTime: "2023-01-23T13:23Z",
-  },
-  {
-    name: "Courtney Henry",
-    email: "courtney.henry@example.com",
-    role: "Designer",
-    imageUrl:
-      "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    lastSeen: "3h ago",
-    lastSeenDateTime: "2023-01-23T13:23Z",
-  },
-  {
-    name: "Tom Cook",
-    email: "tom.cook@example.com",
-    role: "Director of Product",
-    imageUrl:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    lastSeen: null,
-  },
-];
+
 const messages = [
   {
     sender: "Dries Vincent",
@@ -93,43 +40,46 @@ const messages = [
 ];
 function ChatRoom() {
   return (
-    <div className="px-6 pt-14 lg:px-8">
-      <div
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
+    <>
+      <Header />
+      <div className="px-6 pt-14 lg:px-8">
         <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
-      </div>
-      <div className="grid grid-cols-8 gap-4 mx-auto p-2">
-        <div className="col-span-2">
+          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
+        <div className="lg:grid grid-cols-12 gap-4 mx-auto p-2">
+          {/* <div className="col-span-2">
           <ListOfUsers people={people}/>
+        </div> */}
+          <div className="col-span-9">
+            <VideoScreen />
+          </div>
+          <div className="col-span-3">
+            <ChatsScreen messages={messages} />
+          </div>
         </div>
-        <div className="col-span-4">
-          <VideoScreen/>
-        </div>
-        <div className="col-span-2">
-          <ChatsScreen messages = {messages}/>
-        </div>
-      </div>
-      <div
-        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-      >
         <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
-          }}
-        />
+          className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
