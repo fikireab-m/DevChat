c85d60630344c5c54dd68a19868fce5bb93f63ed
fikireab-m <fikireab.meku@gmail.com>
2023-05-28 15:34:43 +0300

    create room page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import ChatRoom from './pages/ChatRoom';
-function App() {  
-  return <>
-  <ChatRoom />
-  </>
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ChatRoom from "./pages/ChatRoom";
+import Header from "./components/Header";
+import JoinOrCreateRoom from "./pages/JoinOrCreateRoom";
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<JoinOrCreateRoom />} />
+        <Route path="/chat" element={<ChatRoom />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="absolute inset-x-0 top-0 z-50">
@@ -16,12 +18,9 @@ export default function Header() {
           </a>
         </div>
         <div className="flex lg:flex-1 lg:justify-end">
-          <button className="flex items-center mx-2 px-4 py-2 font-small text-indigo-600 bg-transparent border border-indigo-600 rounded hover:bg-indigo-600 hover:text-white hover:border-transparent focus:outline-none">
-            <span>Join Room</span>
-          </button>
-          <button className="flex items-center mx-2 px-4 py-2 font-small text-green-600 bg-transparent border border-green-600 rounded hover:bg-green-600 hover:text-white hover:border-transparent focus:outline-none">
+          <Link to={'/'} className="flex items-center mx-2 px-4 py-2 font-small text-green-600 bg-transparent border border-green-600 rounded hover:bg-green-600 hover:text-white hover:border-transparent focus:outline-none">
             <span>Create Room</span>
-          </button>
+          </Link>
         </div>
       </nav>
     </header>
