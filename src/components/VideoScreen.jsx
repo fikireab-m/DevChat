76e12c43df6f4988a1130102fe957afdfdc259c6
fikireab-m <fikireab.meku@gmail.com>
2023-06-04 21:44:01 +0300

    +.env

--- src/components/VideoScreen.jsx
+++ src/components/VideoScreen.jsx
@@ -1,40 +1,14 @@
-// import { useState } from "react";
-// import AgoraUIKit from "agora-react-uikit";
-
-// const VideoScreen = () => {
-//   const [videoCall, setVideoCall] = useState(true);
-//   const rtcProps = {
-//     appId: "4f79278a28834b9982933591d900ff81",
-//     channel: "Test",
-//     token: null,
-//   };
-//   const callbacks = {
-//     EndCall: () => setVideoCall(false),
-//   };
-//   return (
-//     <div className="m-auto">
-//       {videoCall ? (
-//         <div className="flex w-[calc(100vw/2.1)] h-[360px]">
-//           <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
-//         </div>
-//       ) : (
-//         <button onClick={() => setVideoCall(true)}>Join</button>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default VideoScreen;
-
 import { useState } from "react";
 import AgoraUIKit, { layout } from "agora-react-uikit";
+
+const APP_ID = import.meta.env.VITE_APP_ID;
 
 const VideoScreen = () => {
   const [videocall, setVideocall] = useState(true);
   const [isHost, setHost] = useState(true);
   const [isPinned, setPinned] = useState(false);
   const [username, setUsername] = useState("");
-
+  console.log(APP_ID);
   return (
     <div className="flex flex-1 w-[calc(75vw)-100px] h-[calc(90vh)]">
       <div style={styles.videoContainer}>
@@ -53,18 +27,17 @@ const VideoScreen = () => {
             </div>
             <AgoraUIKit
               rtcProps={{
-                appId: "4f79278a28834b9982933591d900ff81",
-                channel: "test",
+                appId: APP_ID,
+                channel: "TestChannel",
                 token: null, // add your token if using app in secured mode
                 role: isHost ? "host" : "audience",
                 layout: isPinned ? layout.pin : layout.grid,
                 enableScreensharing: true,
-                grid:2
               }}
               rtmProps={{ username: username || "user", displayUsername: true }}
               callbacks={{
                 EndCall: () => setVideocall(false),
-                ChageView:()=>setPinned(!isPinned),
+                ChageView: () => setPinned(!isPinned),
               }}
             />
           </>
